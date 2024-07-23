9fbf32fa8857e0a4d519cea11986845b378749d9
kayode96-max <67349260+kayode96-max@users.noreply.github.com>
2024-07-23 15:45:46 +0100

    full reupload miners page still missing

---
+++ resume/miners.js
@@ -0,0 +1,18 @@
+function addminers(){
+    var popup = document.getElementById("addminer")
+    var backdrop = document.getElementById('backdrop');
+
+    popup.style.display = 'flex';
+    backdrop.style.display = 'block';
+
+}
+
+function closePopup(){
+    var popup = document.getElementById("addminer")
+    var backdrop = document.getElementById('backdrop');
+
+    popup.style.display = 'none';
+    backdrop.style.display = 'none';
+    console.log('na you no kno work')
+
+}
