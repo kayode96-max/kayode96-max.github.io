69978526ed3ae8762a9edb537ad8158141ca30cc
Usman Abdullahi Olukayode <olukayodeusman406@gmail.com>
2024-07-27 01:01:47 +0100

    final upload i guess

--- resume/miners.js
+++ resume/miners.js
@@ -16,3 +16,28 @@ function closePopup(){
     console.log('na you no kno work')
 
 }
+function displayProfile(){
+    var profile = document.getElementById('userlist2')
+    var list = document.getElementById('userlist')
+    var arrow = document.getElementById('back')
+    var analytics = document.getElementById('analytics')
+
+
+    profile.style.display="flex";
+    list.style.display='none'
+    arrow.style.display= 'flex'
+    analytics.style.display = 'none'
+}
+
+
+function hideProfile(){
+    var profile = document.getElementById('userlist2')
+    var list = document.getElementById('userlist')
+    var arrow = document.getElementById('back')
+
+
+    profile.style.display="none";
+    list.style.display='flex'
+    arrow.style.display= 'none'
+    analytics.style.display = 'flex'
+}
